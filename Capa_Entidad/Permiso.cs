96b98f33b420456f2b782151e9e4578721c7b68c
BaryModesto <123104080+BaryModesto@users.noreply.github.com>
2023-10-12 15:56:40 -0500

    feat: agrwgar clase permiso

---
+++ Capa_Entidad/Permiso.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad
+{ 
+    public class Permiso
+    {
+       public int id_permiso { set;get; }
+        public Rol obj_rol{  set;get; }
+        public string nombre_menu {  set;get; }
+        public string fecha_registro { set; get; }
+    }
+}
