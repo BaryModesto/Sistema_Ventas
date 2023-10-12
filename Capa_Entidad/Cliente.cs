19035954ff265d12ac5c3c7335abd8a409dff7bb
BaryModesto <123104080+BaryModesto@users.noreply.github.com>
2023-10-12 16:25:50 -0500

    feat: clase Cliente

---
+++ Capa_Entidad/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad
+{
+    public class Cliente
+    {
+        public int id_cliente {  get; set; }
+        public string documento{ set; get; }
+        public string nombre_completo{ set; get; }
+        public string correo{ set; get; }
+        public string telefono{ set; get; }
+        public bool estado { set; get; }
+        public string fecha_registro { set; get; }
+    }
+}
