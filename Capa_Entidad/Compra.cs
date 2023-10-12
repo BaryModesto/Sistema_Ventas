b8b4fe5dd89a6d11779b3fd282fbd9a69b74c3a4
BaryModesto <123104080+BaryModesto@users.noreply.github.com>
2023-10-12 16:31:32 -0500

    feat: clase Compra

---
+++ Capa_Entidad/Compra.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad
+{
+    public class Compra
+    {
+        public int id_compra { set; get; }
+        public Usuario obj_usuario { set; get; }
+        public Proveedor obj_proveedor { set; get; }
+        public string tipo_documento { set; get; }
+        public string numero_documento { set; get; }
+        public decimal monto_total {  set; get; }
+        public string fecha_registro { set; get; }
+        
+    }
+}
