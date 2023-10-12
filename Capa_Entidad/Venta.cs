a89be31c13daa1561b107904bd949e72526ada8c
BaryModesto <123104080+BaryModesto@users.noreply.github.com>
2023-10-12 16:43:18 -0500

    feat: clase Venta

---
+++ Capa_Entidad/Venta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad
+{
+    public class Venta
+    {
+        public int id_venta { set; get; }
+        public Usuario obj_usuario { set; get; }        
+        public string tipo_documento { set; get; }
+        public string numero_documento { set; get; }
+        public string documento_cliente { set; get; }
+        public string nombre_cliente { set; get; }
+        public decimal monto_pago { set; get; }
+        public decimal monto_cambio { set; get; }
+        public decimal monto_total { set; get; }
+        public string fecha_registro { set; get; }
+    }
+}
