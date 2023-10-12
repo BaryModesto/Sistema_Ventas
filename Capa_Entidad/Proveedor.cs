f8315fa8281813fb76bfb6a5d6ba7895a44eace2
BaryModesto <123104080+BaryModesto@users.noreply.github.com>
2023-10-12 16:07:41 -0500

    feat: clase proveedor

---
+++ Capa_Entidad/Proveedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad
+{
+    internal class Proveedor
+    {
+        public int id_proveedor { set; get; }
+        public string documento { set; get; }
+        public string razon_social{ set; get; }
+        public string correo{ set; get; }
+        public string telefono{ set; get; }
+        public bool estado { set; get; }
+        public string fecha_registro { set; get; }
+    }
+}
