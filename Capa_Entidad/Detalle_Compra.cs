cd3f4196c2e02d2252dda2d899b5ac2cd283859b
BaryModesto <123104080+BaryModesto@users.noreply.github.com>
2023-10-12 16:36:03 -0500

    feat: clase Detalle Compra

---
+++ Capa_Entidad/Detalle_Compra.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad
+{
+    public class Detalle_Compra
+    {
+        public int id_detalle_c { set; get; }
+        public Compra obj_compra { set; get; }
+        public Producto obj_producto { set; get; }
+        public decimal precio_compra { set; get; }
+        public decimal precio_venta { set; get; }
+        public int cantidad {  set; get; }
+        public decimal monto_total { set; get; }
+        public string fecha_registro { set; get; }
+
+    }
+}
