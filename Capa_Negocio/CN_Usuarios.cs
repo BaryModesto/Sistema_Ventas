55d262ddbdbdd7e3bf7ee15b62c0ae9e05416cb5
BaryModesto <123104080+BaryModesto@users.noreply.github.com>
2023-10-12 20:48:16 -0500

    feat: clase CN_Usuarios
    Cracion de un metodo

---
+++ Capa_Negocio/CN_Usuarios.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capa_Datos;
+using Capa_Entidad;
+
+namespace Capa_Negocio
+{
+    public class CN_Usuarios
+    {
+        private CD_Usuarios cd_usuarios =  new CD_Usuarios();
+        //--- 
+        /*
+         * Dentro de la clase  CD_Usuarios, hay un metodo llamado Listar(), el cual accede a la base de datos, y se encarga de devolver una Lista de Usuarios
+         * El metodo Listar de esta clase, retorna el contenido del metod anteriormente descrito
+         */
+        public List<Usuario> Listar()
+        {
+            return cd_usuarios.Listar();
+        }
+    }
+}
