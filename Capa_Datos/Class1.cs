1681c7f05459795881e62fe7b1ea263962df368c
BaryModesto <123104080+BaryModesto@users.noreply.github.com>
2023-10-12 19:03:38 -0500

    build: reacomodar los proyectos dentro de la solucion

--- Capa_Datos/Class1.cs
+++ Capa_Datos/Class1.cs
@@ -1,4 +1,10 @@
-﻿namespace Capa_Datos
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Datos
 {
     public class Class1
     {
--- Capa_Negocio/Class1.cs
+++ Capa_Negocio/Class1.cs
@@ -1,4 +1,10 @@
-﻿namespace Capa_Negocio
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Negocio
 {
     public class Class1
     {
