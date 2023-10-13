d8460daf81ea94463dbb9ceeb086c3055620577d
BaryModesto <123104080+BaryModesto@users.noreply.github.com>
2023-10-12 19:26:26 -0500

    build: adicionando referencias entre proyectos

--- Capa_Datos/CD_Usuarios.cs
+++ Capa_Datos/CD_Usuarios.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Capa_Entidad;
 
 namespace Capa_Datos
 {
     public class CD_Usuarios
     {
 
-        public List<Usu>
+        public List<Usuario>
     }
 }
