b9c1bdf9d3ac9e958e3579a99e57b73ae0617d49
BaryModesto <123104080+BaryModesto@users.noreply.github.com>
2023-10-12 19:58:00 -0500

    fix: se habia puesto el nombre de un atributo

--- Capa_Entidad/Usuario.cs
+++ Capa_Entidad/Usuario.cs
@@ -9,7 +9,7 @@ namespace Capa_Entidad
     public class Usuario
     {
         public int id_usuario { set; get; }
-        public string usuario { set; get;}
+        public string documento { set; get;}
         public string nombre_completo { set;get; }
         public string correo { set; get;}
         public string clave { set; get; }
