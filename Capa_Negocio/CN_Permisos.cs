758e6ea81bb75e01567a0ae07290fe8727aa1774
BaryModesto <123104080+BaryModesto@users.noreply.github.com>
2023-11-29 00:25:48 -0500

    feat: CN_Permisos
    creacion de clase

---
+++ Capa_Negocio/CN_Permisos.cs
@@ -0,0 +1,21 @@
+﻿using Capa_Datos;
+using Capa_Entidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Negocio
+{
+    public class CN_Permisos
+    {
+        private CD_Permisos cd_permisos = new CD_Permisos();
+      
+        public List<Permiso> Listar(int _usuario)
+        {
+            return cd_permisos.Listar(_usuario);
+        }
+
+    }
+}
