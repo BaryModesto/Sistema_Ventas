83c14a8bc637ec224a9da1f46ccc8c0cee241ea3
BaryModesto <123104080+BaryModesto@users.noreply.github.com>
2023-11-28 22:46:53 -0500

    perf: Abrir_Formulario( )
    los formularios se muestran en la misma ventana de inicio

--- Capa_Presentacion/Inicio.cs
+++ Capa_Presentacion/Inicio.cs
@@ -8,12 +8,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FontAwesome.Sharp;
 
 namespace Capa_Presentacion
 {
     public partial class Inicio : Form
     {
         private static Usuario usuario_actual ;
+        private static IconMenuItem menu_activo = null;
+        private static Form formulario_activo = null;
         public Inicio(Usuario _usuario)
         {
             usuario_actual = _usuario;
@@ -23,14 +26,30 @@ namespace Capa_Presentacion
         {
             label_user_logeado.Text = usuario_actual.nombre_completo;
         }
-        private void Abrir_Formulario(Form _form)
+        private void Abrir_Formulario(IconMenuItem _menu,Form _form)
         {
-            this.Hide();
+            if (menu_activo != null)
+            {
+                menu_activo.BackColor = Color.White;
+            }
+            _menu.BackColor = Color.Silver;
+            menu_activo = _menu;
+
+            if (formulario_activo != null)
+            {
+                formulario_activo.Close();
+            }
+            formulario_activo = _form;
+            _form.TopLevel = false;
+            _form.FormBorderStyle = FormBorderStyle.None;
+            _form.Dock = DockStyle.Fill;
+            _form.BackColor = Color.SteelBlue;
+            contenedor.Controls.Add(_form);
             _form.Show();
         }
         private void menu_usuario_Click(object sender, EventArgs e)
         {
-            Abrir_Formulario(new Form_Usuarios());
+            Abrir_Formulario( (IconMenuItem)sender, new Form_Usuarios());
         }
         
     }
